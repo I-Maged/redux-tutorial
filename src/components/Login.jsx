c2c3dce1b9c44d4de338f3f1de11ee753a17b78b
I-Maged <magedhanafy@gmail.com>
2022-12-22 16:01:08 +0000

    user state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { useDispatch } from "react-redux"
+import { login,logout } from "../features/user"
 
 const Login = () => {
+  const dispatch = useDispatch()
+
   return (
     <div>
       <h1>Login</h1>
-      <button>Login</button>
+      <button onClick={()=>{dispatch(login({name: 'Maged',
+      age: 35,
+      email: 'maged@email.com',}))}}>Login</button>
+      <button onClick={()=>dispatch(logout())}>logout</button>
     </div>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const Profile = () => {
+const user = useSelector((state)=>state.user.value)
+
   return (
     <div>
       <h2>Profile Details:</h2>
-      <p>Name:</p>
-      <p>Age:</p>
-      <p>Email:</p>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>Email: {user.email}</p>
     </div>
   )
 }
