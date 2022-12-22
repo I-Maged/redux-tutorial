877f87229a436a87d2301ae2cb1f91f734fbc724
I-Maged <magedhanafy@gmail.com>
2022-12-22 12:56:59 +0000

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile Details:</h2>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Email:</p>
+    </div>
+  )
+}
+
+export default Profile
